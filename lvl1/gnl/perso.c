1e53e1e6bd085355305bc9fd513fa3e4b27ccf10
Oswen1623 <lucie.nguyen0610@gmail.com>
2026-07-08 20:40:25 +0200

    oui

--- lvl1/gnl/perso.c
+++ lvl1/gnl/perso.c
@@ -2,28 +2,29 @@
 
 char	*ft_strchr(char *s, int c)
 {
+	int	i;
+
+	i = 0;
 	if (!s)
 		return (NULL);
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
+	while (s[i] != c && s[i])
+		i++;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 	return (NULL);
 }
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t i;
+	size_t	i;
 
+	i = 0;
 	if (n == 0 || dest == src)
 		return (dest);
 	if (!dest && !src)
 		return (NULL);
-	i = 0;
 	while (i < n)
 	{
 		((unsigned char *)dest)[i] = ((const unsigned char *)src)[i];
@@ -34,7 +35,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 size_t	ft_strlen(char *s)
 {
-	size_t res;
+	size_t	res;
 
 	res = 0;
 	if (!s)
@@ -46,15 +47,15 @@ size_t	ft_strlen(char *s)
 
 int	str_append_mem(char **s1, char *s2, size_t size2)
 {
-	size_t size1;
-	char *tmp;
+	size_t	size1;
+	char	*tmp;
 
+	size1 = 0;
 	if (!s2 && size2 != 0)
 		return (0);
-	size1 = 0;
 	if (*s1)
 		size1 = ft_strlen(*s1);
-	tmp = malloc(size2 + size1 + 1);
+	tmp = malloc((size2 + size1) * sizeof(char));
 	if (!tmp)
 		return (0);
 	if (size1 > 0)
@@ -62,7 +63,8 @@ int	str_append_mem(char **s1, char *s2, size_t size2)
 	if (size2 > 0)
 		ft_memcpy(tmp + size1, s2, size2);
 	tmp[size1 + size2] = '\0';
-	free(*s1);
+	if (*s1 && s1)
+		free(*s1);
 	*s1 = tmp;
 	return (1);
 }
@@ -74,67 +76,61 @@ int	str_append_str(char **s1, char *s2)
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	unsigned char *d;
-	const unsigned char *s;
+	size_t	i;
 
+	i = 0;
 	if (!dest && !src)
 		return (NULL);
-	d = (unsigned char *)dest;
-	s = (const unsigned char *)src;
-	if (d == s || n == 0)
+	if (dest == src || n == 0)
 		return (dest);
-	if (d > s)
+	if (dest > src)
+		return (ft_memcpy(dest, src, n));
+	while (i < n)
 	{
-		i = n;
-		while (i > 0)
-		{
-			i--;
-			d[i] = s[i];
-		}
-	}
-	else
-	{
-		i = 0;
-		while (i < n)
-		{
-			d[i] = s[i];
-			i++;
-		}
+		((char *)dest)[i] = ((char *)src)[i];
+		i++;
 	}
 	return (dest);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	b[BUFFER_SIZE + 1] = "";
-	char		*ret;
+	static char	buffer[BUFFER_SIZE] = "";
 	char		*temp;
-	int			read_ret;
+	int			bytes;
+	char		*ret;
 
+	temp = ft_strchr(buffer, '\n');
+	bytes = 0;
 	ret = NULL;
 	while (1)
 	{
-		temp = ft_strchr(b, '\n');
-		if (temp)
+		temp = ft_strchr(buffer, '\n');
+		if (!temp)
 		{
-			if (!str_append_mem(&ret, b, temp - b + 1))
-				return (free(ret), NULL);
-			ft_memmove(b, temp + 1, ft_strlen(temp + 1) + 1);
+			if (!str_append_str(&ret, buffer))
+				return (NULL);
+			bytes = read(fd, buffer, BUFFER_SIZE);
+			if (bytes < 0)
+				return (NULL);
+			if (bytes == 0)
+			{
+				if (ret && *ret)
+					return (ret);
+				return (NULL);
+			}
+			buffer[bytes] = '\0';
+		}
+		else
+		{
+			if (!str_append_mem(&ret, buffer, temp - buffer + 1))
+			{
+				free(ret);
+				return (NULL);
+			}
+			ft_memmove(buffer, temp + 1, ft_strlen(temp + 1) + 1);
 			return (ret);
 		}
-		if (!str_append_str(&ret, b))
-			return (NULL);
-		read_ret = read(fd, b, BUFFER_SIZE);
-		if (read_ret <= 0)
-		{
-			b[0] = '\0';
-			if (ret && *ret)
-				return (ret);
-			free(ret);
-			return (NULL);
-		}
-		b[read_ret] = '\0';
 	}
 }
 
