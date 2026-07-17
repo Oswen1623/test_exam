2705c7210389587a8508b4cf9c1eb65958e9d8c8
Oswen <lucie.nguyen0610@gmail.com>
2026-07-17 20:04:58 +0200

    oui

--- lvl1/gnl/perso.c
+++ lvl1/gnl/perso.c
@@ -7,33 +7,30 @@ char	*ft_strchr(char *s, int c)
 	i = 0;
 	if (!s)
 		return (NULL);
-	while (s[i] != c && s[i])
+	if(c == '\0')
+		return NULL;
+	while (s[i])
+	{
+		if(s[i] == c)
+			return s + i;
 		i++;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+	}
 	return (NULL);
 }
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	// size_t	i;
+	size_t	i;
 
-	// i = 0;
-	// if (n == 0 || dest == src)
-	// 	return (dest);
-	// if (!dest && !src)
-	// 	return (NULL);
-	// while (i < n)
-	// {
-	// 	((unsigned char *)dest)[i] = ((const unsigned char *)src)[i];
-	// 	i++;
-	// }
-	while (n > 0)
+	i = 0;
+	if (n == 0 || dest == src)
+		return (dest);
+	if (!dest && !src)
+		return (NULL);
+	while (i < n)
 	{
-		((char *)dest)[n - 1] = ((char *)src)[n - 1];
-		n--;
+		((char *)dest)[i] = ((char *)src)[i];
+		i++;
 	}
 	return (dest);
 }
@@ -56,14 +53,19 @@ int	str_append_mem(char **s1, char *s2, size_t size2)
 	char	*tmp;
 
 	size1 = 0;
-	if (!s2 && size2 == 0)
-		return (0);
+	if (size2 == 0)
+		return (1);
 	if (*s1)
 		size1 = ft_strlen(*s1);
 	tmp = malloc((size2 + size1 + 1) * sizeof(char));
 	if (!tmp)
+	{
+		free(*s1);
+		*s1 = NULL;
 		return (0);
-	ft_memcpy(tmp, *s1, size1);
+	}
+	if (*s1)
+		ft_memcpy(tmp, *s1, size1);
 	ft_memcpy(tmp + size1, s2, size2);
 	tmp[size1 + size2] = '\0';
 	free(*s1);
@@ -78,26 +80,23 @@ int	str_append_str(char **s1, char *s2)
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = 0;
 	if (!dest && !src)
 		return (NULL);
 	if (dest == src || n == 0)
 		return (dest);
-	if (dest > src)
+	if (dest < src)
 		return (ft_memcpy(dest, src, n));
-	while (i < n)
+	while (n > 0)
 	{
-		((char *)dest)[i] = ((char *)src)[i];
-		i++;
+		((char *)dest)[n - 1] = ((char *)src)[n - 1];
+		n--;
 	}
 	return (dest);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE] = "";
+	static char	buffer[BUFFER_SIZE + 1] = "";
 	char		*temp;
 	int			bytes;
 	char		*ret;
@@ -105,18 +104,28 @@ char	*get_next_line(int fd)
 	temp = NULL;
 	bytes = 0;
 	ret = NULL;
+
+	if(fd < 0 || BUFFER_SIZE <= 0)
+		return NULL;
 	while (1)
 	{
 		temp = ft_strchr(buffer, '\n');
 		if (!temp)
 		{
 			if (!str_append_str(&ret, buffer))
-				return (free(ret), NULL);
+			{
+				buffer[0] = '\0';
+				return (NULL);
+			}
 			bytes = read(fd, buffer, BUFFER_SIZE);
 			if (bytes < 0)
+			{
+				buffer[0] = '\0';
 				return (free(ret), NULL);
+			}
 			if (bytes == 0)
 			{
+				buffer[0] = '\0';
 				if (ret && *ret)
 					return (ret);
 				return (free(ret), NULL);
@@ -126,7 +135,10 @@ char	*get_next_line(int fd)
 		else
 		{
 			if (!str_append_mem(&ret, buffer, temp - buffer + 1))
-				return (free(ret), NULL);
+			{
+				buffer[0] = '\0';
+				return (NULL);
+			}
 			ft_memmove(buffer, temp + 1, ft_strlen(temp + 1) + 1);
 			return (ret);
 		}
